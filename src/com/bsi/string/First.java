0bd374d8724c241cadef584c8f059b22dc93bcaa
BSI <bsi0153@naver.com>
2022-11-02 16:50:50 +0900

    junit add

--- src/com/bsi/string/First.java
+++ src/com/bsi/string/First.java
@@ -1,4 +1,10 @@
 package com.bsi.string;
 
 public class First {
+
+    public String getStr(){
+        String str = "123";
+        StringBuffer sb = new StringBuffer(str);
+        return sb.reverse().toString();
+    }
 }
--- src/test/com/bsi/string/FirstTest.java
+++ src/test/com/bsi/string/FirstTest.java
@@ -1,7 +1,16 @@
 package com.bsi.string;
 
-import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 class FirstTest {
+
+    @Test
+    public void getStr(){
+        String str = "123";
+        StringBuffer sb = new StringBuffer(str);
+        Assertions.assertEquals("321",sb.reverse().toString());
+    }
 
 }
